b8a12b587bd83e8729ac3cac785824a1db436c90
Ari <ari@miniasp.com>
2021-07-20 10:39:08 +0800

    端口

---
+++ MsSqlTool/MsSqlTool/SqlObject/Base/BaseObject.cs
@@ -0,0 +1,13 @@
+﻿using MsSqlTool.SqlObject.BaseInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsSqlTool.SqlObject.Base
+{
+    public abstract class BaseObject<TDerive> : IBaseObject<TDerive>
+    {
+    }
+}
---
+++ MsSqlTool/MsSqlTool/SqlObject/Sql/SelectObject.cs
@@ -0,0 +1,13 @@
+﻿using MsSqlTool.SqlObject.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsSqlTool.SqlObject.Sql
+{
+    public class SelectObject : BaseSelectObject<SelectObject>
+    {
+    }
+}

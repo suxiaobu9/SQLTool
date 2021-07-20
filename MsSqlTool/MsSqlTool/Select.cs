3e2e4a50eae52536dca43b0f02620d05d85501e0
Ari <ari@miniasp.com>
2021-07-20 09:34:58 +0800

    端口

--- MsSqlTool/MsSqlTool/Select.cs
+++ MsSqlTool/MsSqlTool/Select.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MsSqlTool.SqlObject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace MsSqlTool
 {
     public static class Select
     {
+        public static void From(string tableName)
+        {
+
+        }
     }
 }
